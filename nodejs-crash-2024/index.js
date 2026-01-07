823b1ad64ec7b40255c4cf82dea24f7272ea0a7b
sjtroxel <sjtroxel@protonmail.com>
2026-01-07 14:32:17 -0600

    ES modules (thru 27:20)

--- nodejs-crash-2024/index.js
+++ nodejs-crash-2024/index.js
@@ -4,7 +4,13 @@
 // console.log(document);
 // console.log(process);
 
-const { generateRandomNumber, celsiusToFahrenheit } = require('./utils');
+// const { generateRandomNumber, celsiusToFahrenheit } = require('./utils');
 
-console.log(`Random Number: ${generateRandomNumber()}`);
-console.log(`Celsius to Fahrenheit: ${celsiusToFahrenheit(0)}`);
+// console.log(`Random Number: ${generateRandomNumber()}`);
+// console.log(`Celsius to Fahrenheit: ${celsiusToFahrenheit(0)}`);
+
+// import { getPosts } from "./postController.js";
+import getPosts, { getPostsLength } from "./postController.js";
+
+console.log(getPosts());
+console.log(`Posts Length: ${getPostsLength()}`)
---
+++ nodejs-crash-2024/postController.js
@@ -0,0 +1,13 @@
+const posts = [
+    {id: 1, title: 'Post One'},
+    {id: 2, title: 'Post Two'}
+];
+
+// export const getPosts = () => posts;
+
+const getPosts = () => posts;
+
+export const getPostsLength = () => posts.length;
+
+// export { getPosts };
+export default getPosts;
