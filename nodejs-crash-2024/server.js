dc42407c03ba8d01d7c6912aeb143529b02ac64f
sjtroxel <sjtroxel@protonmail.com>
2026-01-08 08:48:40 -0600

    setup HTTP server w/ NPM scripts and nodemon (thru 41:02)

---
+++ nodejs-crash-2024/server.js
@@ -0,0 +1,18 @@
+import http from 'http';
+const PORT = 8000;
+
+const server = http.createServer((req, res) => {
+    // res.setHeader('Content-Type', 'text/html');
+    // res.statusCode = 404;
+
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    res.end('<h1>Hello World!</h1>')
+    // res.end(JSON.stringify({ message: 'Server Error!' }));
+
+    // res.write('Hello World!');
+    // res.end('<h1>Hello World!!</h1>');
+});
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}!`)
+});
