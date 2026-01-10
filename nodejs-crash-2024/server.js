3f60143e119e36c153a27a474609b97766c236a9
sjtroxel <sjtroxel@protonmail.com>
2026-01-10 08:53:03 -0600

    a simple server w/ routing (thru 51:50)

--- nodejs-crash-2024/server.js
+++ nodejs-crash-2024/server.js
@@ -1,19 +1,31 @@
 import http from 'http';
-// const PORT = 8000;
 const PORT = process.env.PORT;
 
 const server = http.createServer((req, res) => {
-    // res.setHeader('Content-Type', 'text/html');
-    // res.statusCode = 404;
-    console.log(req.url);
-    console.log(req.method);
+    try {
+        // check if GET request
+        if (req.method === 'GET') {
+            if (req.url === '/') {
+                res.writeHead(200, { 'Content-Type': 'text/html' })
+                res.end('<h1>Homepage</h1>');
+            } else if (req.url === '/about') {
+                res.writeHead(200, { 'Content-Type': 'text/html' })
+                res.end('<h1>About</h1>');        
+            } else {
+                res.writeHead(404, { 'Content-Type': 'text/html' })
+                res.end('<h1>Not Found</h1>');
+            }
+        } else {
+            throw new Error ('Method not allowed!')
+        }
+    } catch (error) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' })
+        res.end('<h1>Server Error</h1>');
+    }
 
-    res.writeHead(200, { 'Content-Type': 'text/html' })
-    res.end('<h1>Hello World!</h1>')
-    // res.end(JSON.stringify({ message: 'Server Error!' }));
 
-    // res.write('Hello World!');
-    // res.end('<h1>Hello World!!</h1>');
+
+
 });
 
 server.listen(PORT, () => {
